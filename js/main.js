d62e249dfc3a37d26ad9363a984b7d846be765cb
ZakariaZack98 <155935860+ZakariaZack98@users.noreply.github.com>
2024-11-26 11:00:31 +0600

    score and progress system refined, scorecard transition effect added, some more questions added

--- js/main.js
+++ js/main.js
@@ -48,6 +48,42 @@ const questionSet = [
       d: "2",
     },
   },
+  {
+    ques: "Which one below is a non-primitive data type?",
+    opt: {
+      a: "true",
+      b: "1",
+      c: "NaN",
+      true: "[2, 4, 5]",
+    },
+  },
+  {
+    ques: "By which syntax we can pick a unique single element from html document?",
+    opt: {
+      a: "document.getElementByClassName('<classname>')",
+      true: "document.getElementById('<id>')",
+      c: "document.querySelector('<.classname>')",
+      d: "All of them above",
+    },
+  },
+  {
+    ques: "which one is true for 'var' keyword",
+    opt: {
+      a: "Global scoped",
+      b: "Can be re-assigned",
+      c: "Can be re-declared",
+      true: "All of them above",
+    },
+  },
+  {
+    ques: "If you want to console.log(a) before declaring let a = 10, what will it return?",
+    opt: {
+      a: "undefined",
+      true: "Will throw an ReferenceError",
+      c: "Will throw an TypeError",
+      d: "None of them",
+    },
+  },
 ];
 
 // !!=============shuffle question on reload====================
@@ -66,12 +102,13 @@ const shuffledQuesSet = shuffle([...questionSet]);
 //selecting an answer============================================
 answerSec.addEventListener("click", (event) => {
   event.stopPropagation();
-  //enabling next button===============================
+  //enabling/disabling areas=========================
   nextBtn.setAttribute("aria-disabled", "false");
   nextBtn.classList.remove("disabled");
+  answerSec.classList.add('cursorDisable');
   //actions if selected answer is correct or incorrect
   if (event.target.getAttribute("correctAnswer") === "true") {
-    score += 20;
+    score += 100 / (questionSet.length + 1);
     event.target.classList.add("bg-success", "text-white");
     Array.from(answerSec.children).forEach((elem) =>
       elem.classList.add("disableArea")
@@ -90,20 +127,21 @@ answerSec.addEventListener("click", (event) => {
 //switching to next question====================================
 nextBtn.addEventListener("click", (event) => {
   event.stopPropagation();
-  //re-disabling nextBtn=======================
+  //enabling/disabling areas===================
   nextBtn.classList.add("disabled");
   nextBtn.setAttribute("aria-disabled", "true");
+  answerSec.classList.remove('cursorDisable');
   //updating progressbar=======================
-  progress += 20;
-  progressLength.style.width = `${progress}%`;
-  progressLength.innerText = `${progress}%`;
+  progress += 100 / (questionSet.length + 1);
+  progressLength.style.width = `${Math.round(progress)}%`;
+  progressLength.innerText = `${Math.round(progress)}%`;
   //aplying flip animation=====================
   answerSec.classList.add("flipOut");
   if(progress >= 100) {
     //creating success page====================
     question.style.display = 'none';
     const successPage = document.createElement('div');
-    successPage.classList.add('successPage', 'd-flex', 'flex-column', 'justify-content-center', 'align-items-center', 'gap-3', 'shadow-lg');
+    successPage.classList.add('successPage', 'd-flex', 'flex-column', 'justify-content-center', 'align-items-center', 'gap-3', 'shadow', 'popUp');
     const congrats = document.createElement('h3');
     congrats.innerText = 'Congratulations!';
     const completion = document.createElement('h5');
@@ -112,13 +150,18 @@ nextBtn.addEventListener("click", (event) => {
     scoreIs.innerText = 'Your score is';
     const finalScore = document.createElement('h1');
     finalScore.classList.add('finalScore')
-    finalScore.innerText = score;
+    finalScore.innerText = Math.round(score);
     //appending elements==========================
     successPage.appendChild(congrats);
     successPage.appendChild(completion);
     successPage.appendChild(scoreIs);
     successPage.appendChild(finalScore);
     canvas.replaceChild(successPage, answerSec);
+    //applying zoom-in effect=====================
+    setTimeout(() => {
+      successPage.classList.remove('popUp');
+      successPage.style.transition = '0.6s ease'
+    }, 100);
   }
   else {
     setTimeout(() => {
@@ -133,7 +176,7 @@ nextBtn.addEventListener("click", (event) => {
         "text-white"
       )
     );
-    //updating DOMs with next question and options
+    //updating DOMs with next question and options===
     question.innerText = shuffledQuesSet[questionAnswered].ques;
     let i = 0;
     for (let option in shuffledQuesSet[questionAnswered].opt) {
